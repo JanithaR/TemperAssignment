20e6aef51c5340f026e814cf638b7f871db28719
JANITHA-DEKSTOP\Janitha <janitha.ranawans@gmail.com>
2018-12-16 19:02:37 +0530

    Improved code for testability

--- app/src/main/java/com/example/janitha/temperassignment/MainActivity.kt
+++ app/src/main/java/com/example/janitha/temperassignment/MainActivity.kt
@@ -6,49 +6,34 @@ import android.support.v7.widget.LinearLayoutManager
 import com.beust.klaxon.Klaxon
 import com.example.janitha.temperassignment.adapters.JobsAdapter
 import com.example.janitha.temperassignment.objects.Job
+import com.example.janitha.temperassignment.utils.DataUtils.Companion.readJSON
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    var firstDayJobs: ArrayList<Job> = ArrayList()
-    var secondDayJobs: ArrayList<Job> = ArrayList()
-    var thirdDayJobs: ArrayList<Job> = ArrayList()
+    var jobs: ArrayList<Job> = ArrayList();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        readJSON("jobsearch.json")
+        loadData()
         init()
+    }
+
+    private fun loadData() {
+        val firstDayJobs: ArrayList<Job> = readJSON("jobsearch.json", application, "2018-05-22")
+        val secondDayJobs: ArrayList<Job> = readJSON("jobsearch.json", application, "2018-05-23")
+        val thirdDayJobs: ArrayList<Job> = readJSON("jobsearch.json", application, "2018-05-24")
+
+        jobs.addAll(firstDayJobs)
+        jobs.addAll(secondDayJobs)
+        jobs.addAll(thirdDayJobs)
     }
 
     private fun init() {
         recyclerViewJobs.layoutManager = LinearLayoutManager(this)
-
-        var allJobs: ArrayList<Job> = ArrayList()
-        allJobs.addAll(firstDayJobs)
-        allJobs.addAll(secondDayJobs)
-        allJobs.addAll(thirdDayJobs)
-
-        recyclerViewJobs.adapter = JobsAdapter(allJobs, this)
-    }
-
-    private fun readJSON(name: String) {
-        val klaxon = Klaxon()
-        val json = application.assets.open(name).bufferedReader().use {
-            it.readText()
-        }
-
-        val jo = JSONObject(json)
-        val firstDay = jo.getJSONObject("data").getJSONArray("2018-05-22").toString()
-        val secondDay = jo.getJSONObject("data").getJSONArray("2018-05-23").toString()
-        val thirdDay = jo.getJSONObject("data").getJSONArray("2018-05-24").toString()
-
-        firstDayJobs = klaxon.parseArray<Job>(firstDay) as ArrayList<Job>
-        secondDayJobs = klaxon.parseArray<Job>(secondDay) as ArrayList<Job>
-        thirdDayJobs = klaxon.parseArray<Job>(thirdDay) as ArrayList<Job>
-
-        println("xxx")
+        recyclerViewJobs.adapter = JobsAdapter(jobs, this)
     }
 }
